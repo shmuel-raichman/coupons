0121d2e474670d3cd4a6adb631f3b44f51bfe683
shmuel <smuel1414@gmail.com>
2018-11-21 01:08:29 +0200

    added - docker image creation

--- src/main/java/shmulik/coupons_manager/final_project/FinalProjectApplication.java
+++ src/main/java/shmulik/coupons_manager/final_project/FinalProjectApplication.java
@@ -2,7 +2,9 @@ package shmulik.coupons_manager.final_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
 public class FinalProjectApplication {
 
