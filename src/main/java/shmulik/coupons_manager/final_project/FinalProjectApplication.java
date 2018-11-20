2cd518d224b3d3242bf5d0f9589256516d2531a6
shmuel <smuel1414@gmail.com>
2018-11-21 01:16:39 +0200

    added - docker image creation

--- src/main/java/shmulik/coupons_manager/final_project/FinalProjectApplication.java
+++ src/main/java/shmulik/coupons_manager/final_project/FinalProjectApplication.java
@@ -2,10 +2,16 @@ package shmulik.coupons_manager.final_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
 
-@Component
+
 @SpringBootApplication
+@ComponentScan({"shmulik.coupons_manager.final_project"})
+@EntityScan("shmulik.coupons_manager.final_project")
+@EnableJpaRepositories("shmulik.coupons_manager.final_project")
 public class FinalProjectApplication {
 
 	public static void main(String[] args) {
