3b86a3e69f70982413ec1cbde139d503341cb823
shmuel <smuel1414@gmail.com>
2018-11-24 22:50:11 +0200

    add docker hub

--- src/main/java/shmulik/coupons_manager/final_project/entities/Company.java
+++ src/main/java/shmulik/coupons_manager/final_project/entities/Company.java
@@ -3,15 +3,29 @@
  */
 package shmulik.coupons_manager.final_project.entities;
 
-import java.util.Date;
+import javax.persistence.*;
 
 /**
  * @author Android
  *
  */
+@Entity
+@Table(name = "coupons" ,uniqueConstraints={@UniqueConstraint(columnNames={"title"})})
 public class Company {
 
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    // TO DO add annotations to make it auto generate table.
+    @Column(name = "comp_name" , unique=true)
+    private String title;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email" , unique=true)
+    private String email;
 	
 	
 }
