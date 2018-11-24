6ae0052fd841be391eacedb62128905813c78dbd
shmuel <smuel1414@gmail.com>
2018-11-24 23:01:49 +0200

    added - company entity

--- src/main/java/shmulik/coupons_manager/final_project/entities/Company.java
+++ src/main/java/shmulik/coupons_manager/final_project/entities/Company.java
@@ -3,15 +3,37 @@
  */
 package shmulik.coupons_manager.final_project.entities;
 
-import java.util.Date;
+import javax.persistence.*;
 
 /**
  * @author Android
  *
  */
+@Entity
+@Table(name = "coupons" )
 public class Company {
 
-	
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "comp_name" , unique=true)
+    private String comp_name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email" , unique=true)
+    private String email;
+
+    public Company() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
